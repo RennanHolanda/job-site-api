869af92825cccc2c9c8341c3243db3841641b598
Bruno Rennan <bruno_rennan@hotmail.com>
2023-08-07 21:07:34 -0300

    cors http

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ import cors from 'cors';
 dotenv.config();
 app.use(express.json())
 
-app.use(cors({ origin: ['http://localhost:3000'] }))
+app.use(cors({ origin: ['http://localhost:3000', 'https://jobdeploy-api.onrender.com'] }))
 
 
 app.use(router)
